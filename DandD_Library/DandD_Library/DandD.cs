d9429121b56b1654f223a985a342d09ee53eca7f
atouchetAIE <cidleaf@gmail.com>
2016-05-03 09:03:51 -0500

    First commit. Initalization

---
+++ DandD_Library/DandD_Library/DandD.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DandD_Library
+{
+    public class Sword : IWeapon
+    {
+        public string weaponName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int range
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int rolls
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int diceRange
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int attackBonus
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool proficient
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Sword(string na, int ra, int ro, int dr)
+        {
+            weaponName = na;
+            range = ra;
+            rolls = ro;
+            diceRange = dr;
+        }
+
+        public int CalculateAttackBonus()
+        {
+            int result = 0;
+            //Get Player's attribue the weapon uses. Calculate
+            if (proficient == true)
+            {
+                result += 2;
+            }
+
+            return result;
+        }
+
+        public List<int> GetDamage()
+        {
+            List<int> attacks = new List<int>();
+            Random ran = new Random();
+
+            for (int i = 0; i < this.rolls; i++)
+            {
+                attacks.Add(ran.Next(0, this.diceRange));
+            }
+
+            return attacks;
+        }
+    }
+
+    public class Character
+    {
+    }
+}
---
+++ DandD_Library/DandD_Library/Interfaces.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DandD_Library
+{
+    public interface IWeapon
+    {
+        string weaponName { get; }  //Name of the weapon
+        int range { get; }         //Range of weapon
+        //Weapon Type
+        //Attack ablilty
+        int rolls { get; }          //Number of rolls for the weapon
+        int diceRange { get; }      //Max range of the dice
+
+        int attackBonus { get; }    //Bonus from attributes
+        bool proficient { get; }    //Is character Proficent
+
+        List<int> GetDamage();
+        int CalculateAttackBonus();
+    }
+}
